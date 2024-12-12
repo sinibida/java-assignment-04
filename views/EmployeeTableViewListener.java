45ea39b30d6b823c84af481477275686a4211a25
SPAUPA <40449398+sinibida@users.noreply.github.com>
2024-12-13 00:23:37 +0900

    feat: EmployeeTableViewListener

--- views/EmployeeTableViewListener.java
+++ views/EmployeeTableViewListener.java
@@ -3,26 +3,89 @@ package views;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Types;
-import java.util.Vector;
+import java.util.Arrays;
 
-import shared.MsgDialog;
+import shared.NumberFormatErrDialog;
 import shared.SQLExceptionDialog;
 import shared.SQLRunner;
 import views.FormDialog.FormDialogListener;
 import views.TableView.TableViewListener;
 
 public class EmployeeTableViewListener implements TableViewListener {
+  static final String[] ALL_COLUMNS = new String[] { "Ssn",
+      "BDate",
+      "Sex",
+      "Address",
+      "Salary",
+      "Fname",
+      "Minit",
+      "Lname",
+      "Supervision",
+      "WorksFor", };
+  static final String[] ID_COLUMNS = new String[] { "Ssn" };
+
+  static final String SELECT_ALL_STATEMENT = "SELECT * FROM EMPLOYEE";
+
+  static final String SELECT_ID_STATEMENT = "SELECT * FROM EMPLOYEE WHERE Ssn=?";
+  static final int[] SELECT_ID_TYPES = new int[] {
+      Types.NUMERIC,
+  };
+  static final String SELECT_ID_TITLE = "변경할 데이터 조회";
+  static final String SELECT_ID_BUTTON = "선택";
+
   static final String INSERT_STATEMENT = "INSERT INTO EMPLOYEE VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+  static final int[] INSERT_TYPES = new int[] {
+      Types.NUMERIC,
+      Types.DATE,
+      Types.VARCHAR,
+      Types.VARCHAR,
+      Types.NUMERIC,
+      Types.VARCHAR,
+      Types.VARCHAR,
+      Types.VARCHAR,
+      Types.NUMERIC,
+      Types.NUMERIC,
+  };
+  static final String INSERT_TITLE = "직원 추가";
+  static final String INSERT_BUTTON = "추가";
+
+  static final String UPDATE_STATEMENT = "UPDATE EMPLOYEE SET Ssn=?, BDate=?, Sex=?, Address=?, Salary=?, Fname=?, Minit=?, Lname=?, Supervision=?, WorksFor=? WHERE Ssn=?";
+  static final int[] UPDATE_TYPES = new int[] {
+      Types.NUMERIC,
+      Types.DATE,
+      Types.VARCHAR,
+      Types.VARCHAR,
+      Types.NUMERIC,
+      Types.VARCHAR,
+      Types.VARCHAR,
+      Types.VARCHAR,
+      Types.NUMERIC,
+      Types.NUMERIC,
+      Types.NUMERIC,
+  };
+  static final String UPDATE_TITLE = "직원 정보 변경";
+  static final String UPDATE_BUTTON = "수정";
+
+  static final String DELETE_STATEMENT = "DELETE FROM EMPLOYEE WHERE Ssn=?";
+  static final int[] DELETE_TYPES = new int[] {
+      Types.NUMERIC,
+  };
 
   SQLRunner runner;
 
   PreparedStatement insertStatement;
+  PreparedStatement selectIdStatement;
+  PreparedStatement updateStatement;
+  PreparedStatement deleteStatement;
 
   public EmployeeTableViewListener(SQLRunner runner) {
     this.runner = runner;
 
     try {
       insertStatement = runner.getPreparedQuery(INSERT_STATEMENT);
+      selectIdStatement = runner.getPreparedQuery(SELECT_ID_STATEMENT);
+      updateStatement = runner.getPreparedQuery(UPDATE_STATEMENT);
+      deleteStatement = runner.getPreparedQuery(DELETE_STATEMENT);
     } catch (SQLException e) {
       new SQLExceptionDialog("Couldn't prepare SQL statements", null, e).setVisible(true);
     }
@@ -30,55 +93,33 @@ public class EmployeeTableViewListener implements TableViewListener {
 
   @Override
   public String[] getColumnNames() {
-    return new String[] { "Ssn",
-        "BDate",
-        "Sex",
-        "Address",
-        "Salary",
-        "Fname",
-        "Minit",
-        "Lname",
-        "Supervision",
-        "WorksFor", };
+    return ALL_COLUMNS;
   }
 
   @Override
   public Object[][] getData() {
-    final String STATEMENT = "SELECT * FROM EMPLOYEE";
     try {
-      return Utils.statementToObjectMatrix(runner, STATEMENT);
+      return Utils.statementToObjectMatrix(runner, SELECT_ALL_STATEMENT);
     } catch (SQLException e) {
-      new SQLExceptionDialog("Couldn't show SQL result.", STATEMENT, e).setVisible(true);
+      new SQLExceptionDialog("Couldn't show SQL result.", SELECT_ALL_STATEMENT, e).setVisible(true);
       return new Object[][] {};
     }
   }
 
   @Override
   public void onInsert() {
-    FormDialog dialog = new FormDialog("직원 추가", getColumnNames(), "추가", new FormDialogListener() {
+    FormDialog dialog = new FormDialog(INSERT_TITLE, ALL_COLUMNS, INSERT_BUTTON, new FormDialogListener() {
       @Override
       public boolean onSubmit(String[] values) {
         try {
-          Utils.runPreparedStatement(insertStatement, new int[] {
-              Types.NUMERIC,
-              Types.DATE,
-              Types.VARCHAR,
-              Types.VARCHAR,
-              Types.NUMERIC,
-              Types.VARCHAR,
-              Types.VARCHAR,
-              Types.VARCHAR,
-              Types.NUMERIC,
-              Types.NUMERIC,
-          }, values);
+          Utils.runPreparedStatement(insertStatement, INSERT_TYPES, values);
           return true;
         } catch (SQLException e) {
           new SQLExceptionDialog("Couldn't run SQL update.", INSERT_STATEMENT + " + " + String.join(", ", values), e)
               .setVisible(true);
           return false;
         } catch (NumberFormatException e) {
-          new MsgDialog("형식 에러", "알맞은 숫자 값을 입력해주세요: \n\n" + e.getMessage()).setVisible(true);
-          ;
+          new NumberFormatErrDialog(e).setVisible(true);
           return false;
         }
       }
@@ -87,14 +128,82 @@ public class EmployeeTableViewListener implements TableViewListener {
     dialog.setVisible(true);
   }
 
+  // This cannot be a local variable because Java(536871575)
+  Object[] selectedRow = null;
+
+  protected Object[] selectRowWithId() {
+    FormDialog idDialog = new FormDialog(SELECT_ID_TITLE, ID_COLUMNS, SELECT_ID_BUTTON,
+        new FormDialogListener() {
+          @Override
+          public boolean onSubmit(String[] values) {
+            try {
+              selectedRow = Utils.selectOne(selectIdStatement, SELECT_ID_TYPES, values);
+              return selectedRow != null;
+            } catch (SQLException e) {
+              new SQLExceptionDialog("Couldn't run SQL update.",
+                  SELECT_ID_STATEMENT + " + " + String.join(", ", values), e)
+                  .setVisible(true);
+              return false;
+            } catch (NumberFormatException e) {
+              new NumberFormatErrDialog(e).setVisible(true);
+              return false;
+            }
+          }
+
+        });
+
+    selectedRow = null;
+    idDialog.setVisible(true);
+    return selectedRow;
+  }
+
   @Override
   public void onDelete() {
-    System.out.println("Update!");
+    Object[] selectedRow = selectRowWithId();
+    if (selectedRow == null)
+      return;
+
+    String[] values = new String[] { selectedRow[0].toString() };
+    try {
+      Utils.runPreparedStatement(deleteStatement, DELETE_TYPES, values);
+    } catch (SQLException e) {
+      new SQLExceptionDialog("Couldn't run SQL update.", DELETE_STATEMENT + " + " + String.join(", ", values), e)
+          .setVisible(true);
+    } catch (NumberFormatException e) {
+      new NumberFormatErrDialog(e).setVisible(true);
+    }
   }
 
   @Override
   public void onUpdate() {
-    System.out.println("Update!");
-  }
+    FormDialog dialog = new FormDialog(UPDATE_TITLE, ALL_COLUMNS, UPDATE_BUTTON, new FormDialogListener() {
+      @Override
+      public boolean onSubmit(String[] values) {
+        // LATER: Abtract this try-catch w/ Abstract Class?
+        try {
+          String[] newValues = Arrays.copyOf(values, values.length + 1);
+          newValues[values.length] = values[0];
+          Utils.runPreparedStatement(updateStatement, UPDATE_TYPES, newValues);
+          return true;
+        } catch (SQLException e) {
+          new SQLExceptionDialog("Couldn't run SQL update.", SELECT_ID_STATEMENT + " + " + String.join(", ", values), e)
+              .setVisible(true);
+          return false;
+        } catch (NumberFormatException e) {
+          new NumberFormatErrDialog(e).setVisible(true);
+          return false;
+        }
+      }
+    });
 
+    Object[] selectedRow = selectRowWithId();
+    if (selectedRow == null)
+      return;
+    String[] initialValue = new String[selectedRow.length];
+    for (int i = 0; i < selectedRow.length; i++) {
+      initialValue[i] = selectedRow[i] == null ? "" : selectedRow[i].toString();
+    }
+    dialog.setInitialValue(initialValue);
+    dialog.setVisible(true);
+  }
 }
