866e7cec647d1ed619636cf974547f063b0cdb31
SPAUPA <40449398+sinibida@users.noreply.github.com>
2024-12-14 21:45:53 +0900

    +: manageStartDate

--- app/MainContentPane.java
+++ app/MainContentPane.java
@@ -19,6 +19,7 @@ import views.DepartmentLocationTableViewListener;
 import views.DepartmentTableViewListener;
 import views.DependentsTableViewListener;
 import views.EmployeeTableViewListener;
+import views.ManageStartDateTableViewListener;
 import views.MeImage;
 import views.ProjectTableViewListener;
 import views.TableView;
@@ -44,33 +45,43 @@ public class MainContentPane extends JPanel {
 
       TableView employeeView = new TableView("직원 정보", new EmployeeTableViewListener(runner));
       TableView dependentView = new TableView("가족 정보", new DependentsTableViewListener(runner));
+
       TableView departmentView = new TableView("부서 정보", new DepartmentTableViewListener(runner));
       TableView departmentLocationView = new TableView("부서 위치", new DepartmentLocationTableViewListener(runner));
+      TableView manageStartDateView = new TableView("부서 관리 시작일", new ManageStartDateTableViewListener(runner));
+
       TableView projectView = new TableView("프로젝트 정보", new ProjectTableViewListener(runner));
 
       // 각 TablweView의 너비 설정
       employeeView.setPreferredSize(new Dimension(800, 0));
       dependentView.setPreferredSize(new Dimension(400, 0));
+
       departmentView.setPreferredSize(new Dimension(250, 0));
       departmentLocationView.setPreferredSize(new Dimension(150, 0));
+      manageStartDateView.setPreferredSize(new Dimension(150, 0));
+      
       projectView.setPreferredSize(new Dimension(400, 0));
 
       // https://stackoverflow.com/a/46541119
       center.add(employeeView);
       center.add(Box.createHorizontalStrut(16));
       center.add(dependentView);
-      center.add(Box.createHorizontalStrut(16));
+      center.add(Box.createHorizontalStrut(32));
+
       center.add(departmentView);
       center.add(Box.createHorizontalStrut(16));
       center.add(departmentLocationView);
       center.add(Box.createHorizontalStrut(16));
+      center.add(manageStartDateView);
+      center.add(Box.createHorizontalStrut(32));
+  
       center.add(projectView);
     }
     JScrollPane scrollCenter = new JScrollPane(
         center,
         JScrollPane.VERTICAL_SCROLLBAR_NEVER,
         JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-    center.setBorder(new EmptyBorder(new Insets(8, 8, 8, 8 )));
+    center.setBorder(new EmptyBorder(new Insets(8, 8, 8, 8)));
     add(scrollCenter, BorderLayout.CENTER);
 
     JPanel south = new JPanel();
